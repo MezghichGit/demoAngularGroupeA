5aec93ce8261a2e7a944ff427b9d26350f2995f1
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-06-18 14:37:09 +0100

    les directives

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Student } from '../entities/Models';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+
+  formateur:string="Mohamed Amine MEZGHICH";
+  noms:string[]=["mohamed adem","adem","wassim","khalil","ali"];
+  totalEtudiants:number=20;
+  test:boolean=true;
+  students:Student[]=[
+    {
+      "nom":"mohamed adem",
+      "age":22,
+      "email":"medadem@gmail.com",
+      "moy":14,
+      "address":"Tunis",
+      "tel":20200300
+    },
+    {
+      "nom":"wassim",
+      "age":21,
+      "email":"wassim@gmail.com",
+      "moy":15,
+      "address":"Tunis",
+      "tel":21100400
+    },
+    {
+      "nom":"khalil",
+      "age":24,
+      "email":"khalil@gmail.com",
+      "moy":13.5,
+      "address":"Souka",
+      "tel":22222333
+    }
+  ];
 
   constructor() { }
 
---
+++ src/app/entities/Models.ts
@@ -0,0 +1,8 @@
+export interface Student {
+  nom:string,
+  age:number,
+  email:string,
+  moy:number,
+  address:string,
+  tel:number
+}
